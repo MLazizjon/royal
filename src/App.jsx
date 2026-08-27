791d64250eb5aeb34006ba524ed7cf9ae9c1f7d5
MLazizjon <maxmudjonovlazizjon@gmail.com>
2026-08-27 19:27:26 +0500

    fix telegram routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,23 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import './style/StyleComponent';
 
-// Komponentlarni to'g'ri yo'l bilan import qilish
 import Login from './pages/adminPage/login/login'; 
-import AdminPanel from './pages/dashboard/AdminPage'; // O'z papkangizga qarab yo'lini to'g'rilang
-// import NotFound from './components/NotFound'; // Agar bo'lmasa, yaratib qo'ying
+import AdminPanel from './pages/dashboard/AdminPage'; 
 
 function App() {
   return (
     <div className="App">
-
       <ToastContainer />
-
 
       <Routes>
         {/* Asosiy sahifa - Login */}
         <Route path="/" element={<Login />} />
         
-        
         {/* Admin Panel yo'li */}
         <Route path="/adminpanel" element={<AdminPanel />} />
 
-        {/* Topilmagan sahifalar uchun (404) */}
-        {/* <Route path="*" element={<NotFound />} /> */}
+        {/* Telegram parametrlari kelganda ham Login sahifasini ochish uchun: */}
+        <Route path="*" element={<Login />} />
       </Routes>
     </div>
   );
